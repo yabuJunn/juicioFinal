72541a89cc1aded0c1a4625786611f5c0ecd3265
yabuJunn <juancho04reyes@gmail.com>
2025-03-10 04:42:04 -0500

    Vista pantalla seleccion de caso terminada

--- src/pages/selectCasePage/selectCasePage.tsx
+++ src/pages/selectCasePage/selectCasePage.tsx
@@ -1,19 +1,39 @@
 import "./selectCasePage.css"
 
 import case1Image from "../../assets/jpg/case1_image.jpg"
+import leftArrow from "../../assets/svg/arrow_left_icon.svg"
+import rightArrow from "../../assets/svg/arrow_right_icon.svg"
 
 export const SelectCasePage = () => {
     return <>
         <section className="page" id="selectCasePage">
+            <div id="selectCasePageBackground">
+                <img src={case1Image} alt="" />
+            </div>
+
+            <div id="selectCasePageBackgroundOpacity">
+
+            </div>
+
             <div className="marginContainer">
-                <h1>Seleccionar Caso</h1>
+                <h1>Seleccionar<br></br>Caso</h1>
 
                 <div id="casesContainer">
                     <h3>El misterio de la mansion centenaria</h3>
                     <div className="caseImageContainer">
                         <img src={case1Image} alt={case1Image} />
                     </div>
+
+                    <div id="casesArrowsContainer">
+                        <img src={leftArrow} alt="" />
+
+                        <img src={rightArrow} alt="" />
+                    </div>
                 </div>
+
+                <button id="selectCaseNextButton">
+                    Siguiente
+                </button>
             </div>
         </section>
     </>
