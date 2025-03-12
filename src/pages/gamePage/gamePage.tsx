00cbbf29050d934a87be1d65f4778897407036ea
yabuJunn <juancho04reyes@gmail.com>
2025-03-12 11:00:44 -0500

    Logica para cambiar de boton de grupo de a y b

--- src/pages/gamePage/gamePage.tsx
+++ src/pages/gamePage/gamePage.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import "./gamePage.css"
+import "./gamePage.css";
 import { NavigationHook } from "../../hooks/navigationHook";
 
-import gamePageBackground from "../../assets/jpg/fondo_gamePage.jpg"
+import gamePageBackground from "../../assets/jpg/fondo_gamePage.jpg";
 
 export const GamePage = () => {
     const navigationHook = NavigationHook();
@@ -11,8 +11,9 @@ export const GamePage = () => {
 
     const [turnNumber, setTurnNumber] = useState<number>(0);
     const [groupTurn, setGroupTurn] = useState<number>(0);
+    const [groupButtonSelected, setGroupButtonSelected] = useState<number>(0); // 0: ninguno, 1: A, 2: B
 
-    return <>
+    return (
         <section className="page" id="gamePage">
             <div id="gamePageBackground">
                 <img src={gamePageBackground} alt="" />
@@ -21,49 +22,64 @@ export const GamePage = () => {
             <div id="gamePageContent">
                 <h1>{caseName}</h1>
 
+                {/* CONTENEDOR TURNO */}
                 <div id="gamePageNextTurnContainer">
                     <div id="gamePageNextTurnContent">
                         <p>Un grupo ha llegado a los <strong>20 puntos</strong></p>
                         <button id="gamePageBuyJudgeButton" onClick={() => { }}>
                             Comprar Juez
                         </button>
-
-                        <button id="gamePageNextTurnButton" onClick={() => {
-                            setTurnNumber(turnNumber + 1)
-                            setGroupTurn(groupTurn + 1)
-                        }}>
+                        <button
+                            id="gamePageNextTurnButton"
+                            onClick={() => {
+                                setTurnNumber((prev) => prev + 1);
+                                setGroupTurn((prev) => prev + 1);
+                                setGroupButtonSelected(0);
+                            }}
+                        >
                             Pasar turno
                         </button>
                     </div>
                 </div>
 
+                {/* CONTENEDOR BOTONES Y PUNTOS */}
                 <div id="gamePageButtonsContainer">
-
                     <div id="turnIndicatorContainer">
                         <div id="turnIndicatorBackground">
-                            <p>Turno: {groupTurn}</p>
+                            <p>Turno: {turnNumber}</p>
                         </div>
 
                         <div id="turnIndicatorGroups">
-                            <button className="turnIndicatorGroup">
+                            {/* BOTÓN A */}
+                            <button
+                                className={`turnIndicatorGroup ${groupButtonSelected === 1 ? "selected" : ""}`}
+                                onClick={() => setGroupButtonSelected(1)}
+                            >
                                 A
                             </button>
 
+                            {/* PUNTOS */}
                             <div id="turnIndicatorNumber">
                                 <p>2<b> : </b>3</p>
                                 <p>Puntos</p>
                             </div>
 
-                            <button className="turnIndicatorGroup">
+                            {/* BOTÓN B */}
+                            <button
+                                className={`turnIndicatorGroup ${groupButtonSelected === 2 ? "selected" : ""}`}
+                                onClick={() => setGroupButtonSelected(2)}
+                            >
                                 B
                             </button>
                         </div>
 
+                        {/* DAR PUNTOS */}
                         <button id="givePointsButton" onClick={() => { }}>
                             Dar puntos
                         </button>
                     </div>
 
+                    {/* BOTONES DE NAVEGACIÓN */}
                     <div id="gamePageNavigationButtons">
                         <div id="gamePageNavigationButtonsSuperior">
                             <button id="gamePageWitnessesButton" onClick={navigationHook.goToTestimoniesPage}>
@@ -79,9 +95,8 @@ export const GamePage = () => {
                             Intervenir
                         </button>
                     </div>
-
                 </div>
             </div>
         </section>
-    </>
-}
+    );
+};
